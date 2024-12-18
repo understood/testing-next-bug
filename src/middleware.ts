80a942c03e31d6195f5cb8d2f450d0fb21f40f8f
Leandro Lima <leandro.mateus.lima@gmail.com>
2024-12-18 14:30:51 -0300

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-    request.nextUrl.pathname = '/en/111'
+    request.nextUrl.pathname = '/en/111' + request.nextUrl.pathname
     return NextResponse.rewrite(request.nextUrl)
 }
