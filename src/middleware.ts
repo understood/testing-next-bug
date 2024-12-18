fed4f66bccdbe93cce0afc0d24d607a2b737d0c7
Leandro Lima <leandro.mateus.lima@gmail.com>
2024-12-18 14:31:35 -0300

    Try deleting the middlware

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function middleware(request) {
-    request.nextUrl.pathname = '/en/111' + request.nextUrl.pathname
-    return NextResponse.rewrite(request.nextUrl)
-}
